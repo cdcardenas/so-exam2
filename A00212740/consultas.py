70a6cc833e127f990960225070d2681eaf1e1d9f
Christian <crisdavcar@gmail.com>
2017-05-05 12:44:00 -0500

    Update consultas.py

--- A00212740/consultas.py
+++ A00212740/consultas.py
@@ -5,14 +5,15 @@ from comandos import get_all_stats, get_cpu, get_service, get_hdd
 
 
 db.create_all()
-
+#se capturan los datos según el formato indicado en modelo.py
 stats = Stats(get cpu()[2], get_all_stats()[1], get_hdd()[1], get_service() )
 
 
-
+#se agregan los cambios en la base de datos
 db.session.add(stats)
+#se guardan los cambios en la base de datos
 db.session.commit()
 
-stats2 = Stats.query.all()
-print(stats2)
+#stats2 = Stats.query.all()
+#print(stats2)
 
